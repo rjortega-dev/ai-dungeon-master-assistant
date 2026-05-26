ebe5d02f4f2ad9a66eb908b45a270609682bd4ab
Matthew Garza <matt.garza34@gmail.com>
2026-05-26 17:34:26 -0600

    get campaign function. -need to implement logic to switch adapters for prisma based on environement

--- src/lib/prisma/prisma.ts
+++ src/lib/prisma/prisma.ts
@@ -1,12 +1,12 @@
 import "dotenv/config";
-import { PrismaNeon } from "@prisma/adapter-neon"
+//import { PrismaNeon } from "@prisma/adapter-neon"
 import { PrismaClient } from "../../../generated/prisma";
 import dotenv from "dotenv";
+import { PrismaPg } from "@prisma/adapter-pg";
 
 dotenv.config();
 const connectionString = `${process.env.DATABASE_URL}`;
-
-const adapter = new PrismaNeon({ connectionString });
+const adapter = new PrismaPg({ connectionString });
 const prisma = new PrismaClient({ adapter });
 
 export { prisma };
